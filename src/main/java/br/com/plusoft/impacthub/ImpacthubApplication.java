fddfeba20ebf3f06fe0c0f511e818c015f14eab7
Bruno Ciccio <dev.bruno.ciccio@gmail.com>
2024-09-10 00:15:21 -0300

    configurações no properties em relação as credenciais de segurança para acesso do localhost e swagger

--- src/main/java/br/com/plusoft/impacthub/ImpacthubApplication.java
+++ src/main/java/br/com/plusoft/impacthub/ImpacthubApplication.java
@@ -2,12 +2,22 @@ package br.com.plusoft.impacthub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class ImpacthubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImpacthubApplication.class, args);
 	}
+
+	@RequestMapping("/home")
+    @ResponseBody
+    public String home() {
+        return "API Back-End do Projeto ImpactHub: Challenge da Plusoft ";
+    }
 
 }
