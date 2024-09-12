71825afe385f090c65a84c45a2e8788cae6015f9
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-09-12 12:35:01 -0300

    teste do CRUD relacionado a Cadastro e export do arquivo do Insomnia

--- src/main/java/br/com/plusoft/impacthub/controller/CadastroController.java
+++ src/main/java/br/com/plusoft/impacthub/controller/CadastroController.java
@@ -1,9 +1,5 @@
 package br.com.plusoft.impacthub.controller;
 
-import static org.springframework.http.HttpStatus.*;
-
-import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,16 +7,30 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import br.com.plusoft.impacthub.assembler.CadastroModelAssembler;
 import br.com.plusoft.impacthub.model.Cadastro;
 import br.com.plusoft.impacthub.repository.CadastroRepository;
-import br.com.plusoft.impacthub.assembler.CadastroModelAssembler;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/cadastro")
@@ -49,20 +59,21 @@ public class CadastroController {
         return pagedAssembler.toModel(page, assembler);
     }
 
-    @GetMapping("{id}")
-    @Operation(summary = "Buscar Cadastro por ID", description = "Retorna um cadastro específico pelo seu ID")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "Cadastro retornado com sucesso"),
-        @ApiResponse(responseCode = "404", description = "Cadastro não encontrado"),
-        @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
-    })
-    public EntityModel<Cadastro> show(@PathVariable Long id) {
-        log.info("Buscando cadastro com id {}", id);
-        var cadastro = repository.findById(id).orElseThrow(
-            () -> new IllegalArgumentException("Cadastro não encontrado")
-        );
-        return assembler.toModel(cadastro);
-    }
+@GetMapping("{id}")
+@Operation(summary = "Buscar Cadastro por ID", description = "Retorna um cadastro específico pelo seu ID")
+@ApiResponses({
+    @ApiResponse(responseCode = "200", description = "Cadastro retornado com sucesso"),
+    @ApiResponse(responseCode = "404", description = "Cadastro não encontrado"),
+    @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+})
+public EntityModel<Cadastro> show(@PathVariable Long id) {
+    log.info("Buscando cadastro com id {}", id);
+    var cadastro = repository.findById(id).orElseThrow(
+        () -> new ResponseStatusException(NOT_FOUND, "Cadastro não encontrado")
+    );
+    return assembler.toModel(cadastro);
+}
+
 
     @PostMapping
     @ResponseStatus(CREATED)
@@ -92,26 +103,26 @@ public class CadastroController {
     public ResponseEntity<EntityModel<Cadastro>> update(@PathVariable Long id, @RequestBody @Valid Cadastro cadastro) {
         log.info("Atualizando cadastro com id {}", id);
         repository.findById(id).orElseThrow(
-            () -> new IllegalArgumentException("Cadastro não encontrado")
+            () -> new ResponseStatusException(NOT_FOUND, "Cadastro não encontrado")
         );
         cadastro.setIdUsuario(id);
         repository.save(cadastro);
         return ResponseEntity.ok(assembler.toModel(cadastro));
     }
-
+    
     @DeleteMapping("{id}")
-    @ResponseStatus(NO_CONTENT)
-    @Operation(summary = "Excluir um Cadastro", description = "Exclui um cadastro existente pelo seu ID")
-    @ApiResponses({
-        @ApiResponse(responseCode = "204", description = "Cadastro excluído com sucesso"),
-        @ApiResponse(responseCode = "404", description = "Cadastro não encontrado"),
-        @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
-    })
-    public void destroy(@PathVariable Long id) {
-        log.info("Excluindo cadastro com id {}", id);
-        repository.findById(id).orElseThrow(
-            () -> new IllegalArgumentException("Cadastro não encontrado")
-        );
-        repository.deleteById(id);
-    }
+@ResponseStatus(NO_CONTENT)
+@Operation(summary = "Excluir um Cadastro", description = "Exclui um cadastro existente pelo seu ID")
+@ApiResponses({
+    @ApiResponse(responseCode = "204", description = "Cadastro excluído com sucesso"),
+    @ApiResponse(responseCode = "404", description = "Cadastro não encontrado"),
+    @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+})
+public void destroy(@PathVariable Long id) {
+    log.info("Excluindo cadastro com id {}", id);
+    repository.findById(id).orElseThrow(
+        () -> new ResponseStatusException(NOT_FOUND, "Cadastro não encontrado")
+    );
+    repository.deleteById(id);
+}
 }
